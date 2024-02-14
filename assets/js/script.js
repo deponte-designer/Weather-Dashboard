67a05e6db1382dcd11c9cb9e86d4120276a8f4b1
Ponte, Nadia <deponte.work@gmail.com>
2024-02-14 15:31:13 +0000

    Make an AJAX request to the OpenWeatherMap API

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,19 @@ function getWeatherByCity(cityName) {
   // Construct the API URL
   const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+// Make an AJAX request to the OpenWeatherMap API
+$.ajax({
+    url: apiUrl,
+    method: "GET",
+  })
+    .then(function (response) {
+      displayCurrentWeather(response);
+      displayForecast(response);
+      addToHistory(cityName);
+    })
+    .fail(function (error) {
+      // Handle errors by logging to the console and displaying an alert
+      console.error("Error fetching weather data:", error);
+      alert(`${cityName} doesn't exist`);
+    });
 }
