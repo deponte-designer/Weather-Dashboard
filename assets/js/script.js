f61df926838e980e0b8a8aae61ae8f4c2b1b4303
Ponte, Nadia <deponte.work@gmail.com>
2024-02-14 15:32:11 +0000

    Function to display current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,53 @@ $("#search-form").on("submit", function (event) {
     // Clear the input field
     userInput.val("");
   });
+
+  // Function to display current weather data
+function displayCurrentWeather(response) {
+    const currentDay = $("#today");
+    currentDay.empty();
+    $("h4").removeClass("hide");
+  
+    const currentWeather = response.list[0];
+    const cityName = response.city.name;
+    const date = dayjs.unix(currentWeather.dt).format("DD MMM, YYYY");
+    const icon = currentWeather.weather[0].icon;
+    const temperature = currentWeather.main.temp;
+    const humidity = currentWeather.main.humidity;
+    const windSpeed = currentWeather.wind.speed;
+  
+    // Generate HTML elements to visually represent data on a webpage
+    const container = $("<div>");
+    const heading = $("<h2>");
+    const iconEl = $("<img>");
+    const temperatureParagraph = $("<p>");
+    const humidityParagraph = $("<p>");
+    const windParagraph = $("<p>");
+  
+    iconEl.attr("src", `https://openweathermap.org/img/wn/${icon}.png`);
+    heading.text(`${cityName} (${date})`);
+    heading.append(iconEl);
+    temperatureParagraph.text(`Temperature: ${temperature} °C`);
+    humidityParagraph.text(`Humidity: ${humidity} %`);
+    windParagraph.text(`Wind Speed: ${windSpeed} m/s`);
+    currentDay.addClass("border");
+    currentDay.css({
+      "background-color": "#fff",
+      "padding": "20px",
+      "margin-bottom": "20px",
+      "border-radius": "8px",
+      "box-shadow": "0 0 10px rgba(0, 0, 0, 0.1)",})
+  
+  
+    container.append(
+      heading,
+      temperatureParagraph,
+      humidityParagraph,
+      windParagraph
+    );
+  
+    currentDay.css("padding-left", "1rem");
+    currentDay.append(container);
+  }
+
+  
