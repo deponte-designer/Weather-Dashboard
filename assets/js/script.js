cfa1b498477c95e0f65f21f504bfd95f127fbf20
Ponte, Nadia <deponte.work@gmail.com>
2024-02-14 15:30:37 +0000

    function api

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const apiKey = "e063538dbd7a64e6085495b882b71be5";
+
+// Function to fetch weather data by city name
+function getWeatherByCity(cityName) {
+  // Construct the API URL
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
